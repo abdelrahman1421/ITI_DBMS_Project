152131fade616402d2765c2572865f88af0dc139
abdelrahman <abdelrahman.eng_3540@eng.kfs.edu.eg>
2022-09-04 18:35:08 +0200

    Adding function to check the user input if it contains special characters and after that remove spaces

--- createDB.sh
+++ createDB.sh
@@ -2,17 +2,24 @@
 
 create_dbm () {
 		read -p "Please enter Database name: " db_name
+		chdb=` echo $db_name | tr -d "[:space:]" `		#this command is to remove spaces form entered name from user#
+		echo $chdb
+		
+		check=` find ./DBMS -type d -name $chdb `
 
-		check=`find ./DBMS -type d -name $db_name`
-
-		if [ "$check" = "./DBMS/$db_name" ]
+		if [[ "$chdb" != *[!a-zA-Z]* ]]
 		then
-       			 echo -e "erorr! $db_name is already exist. \n" 
+			echo not valid
+		fi
+
+		if [ "$check" = "./DBMS/$chdb" ]
+		then
+       			 echo -e "erorr! $chdb is already exist. \n" 
 		else
 
-       			mkdir ./DBMS/$db_name
+       			mkdir ./DBMS/$chdb
 
-        		echo -e "Database $db_name created successfulty. \n"
+        		echo -e "Database $chdb created successfulty. \n"
 
 		fi
 }
---
+++ handeling_user_input.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -p "Please enter Database name: " db_name
+chdb=` echo $db_name | tr -d "[:space:]" `		#this command is to remove spaces form entered name from user#
+
+#This condition is to check if the entered name has special chracters or no
+
+if [[ $chdb =~ ['!@#$%^&*()_+'] ]]; 
+then
+    echo yes
+else
+    echo no
+fi
