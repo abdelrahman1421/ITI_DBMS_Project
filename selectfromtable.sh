e471bc7d3e9c724698f8daf711a927ef8341f372
abdelrahman1421 <abdelrahman.eng_3540@eng.kfs.edu.eg>
2022-09-09 10:45:33 +0200

    select now is ready to use

--- selectfromtable.sh
+++ selectfromtable.sh
@@ -1,16 +1,54 @@
 #!/bin/bash
 
+####################################
+
+function condition {
+  echo -e "Select all columns that matches the enterd operator \n"
+  echo -e "Enter required Column name: \c"
+  read c_name
+  c_id=` awk 'BEGIN{FS=":"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$c_name'") print i}}}' $t_name `
+  if [[ $c_id == "" ]]
+  then
+    echo "Not Found"
+    selecetFromTable
+  else
+  
+    echo -e "You can chose from this supported operators: [ ==, !=, >, <, >=, <= ] \n Select one: \c"
+    read operat
+    
+    if [[ $operat == "==" ]] || [[ $operat == "!=" ]] || [[ $operat == ">" ]] || [[ $operat == "<" ]] || [[ $operat == ">=" ]] || [[ $operat == "<=" ]]
+    then
+      echo -e "\nEnter required VALUE: \c"
+      read val
+      
+      res=`awk 'BEGIN{FS=":"}{if ( $'$c_id$operat$val' )  print $0 }' $t_name 2>> /dev/null | column -t -s ':'`
+      if [[ $res == "" ]]
+      then
+        echo "Value Not Found"
+        selecetFromTable
+      else
+        awk 'BEGIN{FS=":"}{if ($'$c_id$operat$val') print $0}' $t_name | column -t -s ':'
+        selecetFromTable
+      fi
+    else
+      echo "Unsupported Operator\n"
+      selecetFromTable
+    fi
+  fi
+}
+
+######################################
 typeset -i count=0
 read -p "Enter the table name: " t_name
-echo -e "\n"
 
  cat $t_name 2> /dev/null 1> /dev/null 
 
 function selecetFromTable () {
 if [ $? == 0 ]
-    then 
-    while true
-    do
+    then
+
+while true
+do
          
 echo -n "
 
@@ -18,7 +56,7 @@ echo -n "
     |                                            |
     |                                            |
     |  (1) Select all cloumns                    |
-    |  (2) Select specific column                |
+    |  (2) Select specific column      
     |  (3) Select from table with condition      |
     |  (4) Back to Table Menu                    |
     |  (5) Back to Home Menu                     |
@@ -33,27 +71,32 @@ echo -n "
 
         if [ $REPLY -eq 1 ] 
         then
-                cat $t_name
+                column -t -s ': ' $t_name
 
         elif [ $REPLY -eq 2 ]
         then
-               
+                
                 read -p "ENter the column name: " c_name
-                tnum=`head -3 abdo | tr -s ' ' '\n' | nl -nln |  grep "$c_name" | cut -f1`
-                awk -v var=$tnum '{ print $var}' $t_name
+                c_num=`awk -F: -v col="$c_name" 'NR==1{for (i=1; i<=NF; i++) if ($i==col) {print i;exit}}' $t_name`
+                awk -F: -v val="$c_num" '{ print $val }' $t_name | column -t -s ':'
+                sleep 3
+
 
         elif [ $REPLY -eq 3 ]
         then
-                echo Select from table with condition
-
+                condition
+                
         elif [ $REPLY -eq 4 ]
         then
-                echo Back to Table Menu 
+                cd ../..
+                clear
+                source /home/abdo/project/connectDB.sh
 
-        elif [ $REPLY -eq 5 ]
+        elif [ $REPLY -eq 5]
         then
                 cd ../..
-                source /home/abdo/project/connectDB.sh
+                clear
+                source /home/abdo/project/main_file.sh
                 exit
                 break
         
@@ -83,3 +126,5 @@ done
 } 
 
 selecetFromTable
+
+
