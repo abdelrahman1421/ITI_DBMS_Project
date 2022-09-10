361acb2ce11a11494febb07cb9959a129bd30942
abdelrahman1421 <abdelrahman.eng_3540@eng.kfs.edu.eg>
2022-09-10 14:10:56 +0200

    update function enhancment

--- updateTable.sh
+++ updateTable.sh
@@ -4,32 +4,51 @@
 function updateTable ()
 {
   read -p "Enter the table name: " t_name
+  cat $t_name 2>> /dev/null >> /dev/null 
+  if [ $? -eq 1 ]
+  then 
+      echo -e "\nTable not found \n"
+  else
+  echo ""
+  echo -e "             <<< The table column as following you enter the name of any one >> \n"
+  echo --------------------------------------------------------------------------------------------------------
+    echo ""
+        head -1 $t_name | column -t -s ":"
+    echo ""
+  echo --------------------------------------------------------------------------------------------------------
+    echo ""
   echo -e "Enter column name: \c"
   read c_name
   c_num=`awk 'BEGIN{FS=":"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$c_name'") print i}}}' $t_name 2>>/dev/null`
   if [[ $c_num == "" ]]
   then
     echo "Not Found"
-#tabel menu should placed here
+ #tabel menu should placed here
   else
     echo -e "Enter the value to select row: \c"
     read val
     r_num=`awk 'BEGIN{FS=":"}{if ($'$c_num'=="'$val'") print $'$c_num'}' $t_name 2>>/dev/null`
-    echo ""
-    echo "This current table content"
-    echo "--------------------------------------"
-    echo ""
-    head -1 $t_name >> tem_log
-    grep -w "$val" $t_name >> tem_log
-    column -t -s ":" tem_log
-    echo ""
-    echo "--------------------------------------"
-    echo ""
-    rm -rf tem_log
+
+      if [[ $r_num != "" ]]  
+        then
+            echo ""
+            echo "This current table content"
+            echo --------------------------------------------------------------------------------------------------------
+            echo ""
+            head -1 $t_name >> tem_log
+            grep -w "$val" $t_name >> tem_log
+            column -t -s ":" tem_log
+            echo ""
+            echo "--------------------------------------------------------------------------------------------------------"
+            echo ""
+        else
+          echo ""
+     fi
     if [[ $r_num == "" ]]
     then
+
       echo "Value Not Found"
-#tabel menu should placed here
+ #tabel menu should placed here
     else
       echo -e "Enter which column name to update inside it: \c"
       read sel_column
@@ -37,7 +56,7 @@ function updateTable ()
       if [[ $colu_num == "" ]]
       then
         echo "Not Found"
-#tabel menu should placed here
+ #tabel menu should placed here
       else
         
         echo -e "Enter new value to set: \c"
@@ -47,14 +66,20 @@ function updateTable ()
         sed -i ''$NR's/'$old_val'/'$new_val'/g' $t_name 2>>/dev/null
         echo --------------------------------------------------------------------------------------------------------
         echo ""
-        echo "The value $old_val >>>>>> was updated to $new_val "
+                head -1 $t_name >> tem_data
+                grep -w "$val" $t_name >> tem_data
+                column -t -s ":" tem_data >>dif_tem_data
+                column -t -s ":" tem_log >>dif_tem_log
+                pr -m -t dif_tem_log dif_tem_data
         echo ""
         echo --------------------------------------------------------------------------------------------------------
         echo "Row Updated Successfully"
-#tabel menu should placed here
+        rm -rf tem_data tem_log dif_tem_data dif_tem_log
+ #tabel menu should placed here
       fi
     fi
   fi
+  fi 
 }
 
 updateTable
