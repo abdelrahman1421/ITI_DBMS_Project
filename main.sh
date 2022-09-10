ae8eba30ae65e915bb27a8454c5087c8b7c06ef9
Ahmed-Ali-Elbz <ahmed.ali.elbaz.mohamed@gmail.com>
2022-09-10 16:58:20 +0200

    Update Menus Design

---
+++ main.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+shopt -s extglob
+
+# Hello this will be our main file for DBMS project based on bash script
+
+messg="\n \t Welcome to DBMS based on bash script"
+
+if [ -d DBMS ]
+then
+    echo -e $messg
+else
+    mkdir ./DBMS
+    echo -e $messg
+fi
+
+        echo -n "
+    ################(Database-menu)###############
+   ||                                            ||
+   ||                                            ||
+   || (1) Create Database                        ||
+   || (2) List Databases                         ||
+   || (3) Connect to Database                    ||
+   || (4) Drop Database                          ||
+   || (5) Exit                                   ||
+   ||                                            ||
+   ||                                            ||
+    #############################################
+"
+
+while true
+do
+
+    read -p "Please select a choice: "
+
+        
+        typeset -i REPLY=$REPLY
+
+        if [ $REPLY -eq 1 ] 
+        then
+                # Creating a new Database 
+                source ./createDB.sh
+                
+        elif [ $REPLY -eq 2 ]
+        then 
+                # List all Databases
+                source ./listDBs.sh
+                
+
+        elif [ $REPLY -eq 3 ]
+        then
+                # Connect to Database
+                source ./connectDB.sh
+                
+
+        elif [ $REPLY -eq 4 ]
+        then
+                # Remove Database
+                read -p " Enter Database name: " rmdname 
+
+                case $rmdname in
+
+                    +([A-Za-z]))
+                        
+                            if [ -d ./DBMS/$rmdname ]
+                            then
+                        
+                                rm -rf ./DBMS/$rmdname
+                                echo "Done! $rmdname  database was Deleted.."
+                        
+                            else
+                                echo "There is no Database with this name !!"
+                            fi
+                            ;;      
+        
+                    *)
+
+                            echo "invalid Database name: "
+                            ;;      
+        
+                esac
+                
+
+        elif [ $REPLY -eq 5 ]
+        then
+                exit
+                break
+
+        else
+                
+                echo "Please select a number from the Menu" 
+        fi
+
+done
+
+
+
+
